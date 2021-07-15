503a5c789ca92d7017bedae2c3c3a7b1d01733f9
Javi <javi@jsrois.net>
2021-07-15 18:44:35 +0200

    Rename project

--- src/main/java/net/econotravel/econotravelapp/FruitStoreStarterCodeApplication.java
+++ src/main/java/net/econotravel/econotravelapp/FruitStoreStarterCodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BcnfemtechEconotravelStarterCodeApplication {
+public class FruitStoreStarterCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BcnfemtechEconotravelStarterCodeApplication.class, args);
+        SpringApplication.run(FruitStoreStarterCodeApplication.class, args);
     }
 
 }
