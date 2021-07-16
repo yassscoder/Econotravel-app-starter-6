5c21718c4558bd333fe0433c17a2be863a16cdf9
Javi <javi@jsrois.net>
2021-07-16 09:12:57 +0200

    Revert "Rename project"
    This reverts commit 503a5c789ca92d7017bedae2c3c3a7b1d01733f9.

--- src/main/java/net/econotravel/econotravelapp/BcnfemtechEconotravelStarterCodeApplication.java
+++ src/main/java/net/econotravel/econotravelapp/BcnfemtechEconotravelStarterCodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FruitStoreStarterCodeApplication {
+public class BcnfemtechEconotravelStarterCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FruitStoreStarterCodeApplication.class, args);
+        SpringApplication.run(BcnfemtechEconotravelStarterCodeApplication.class, args);
     }
 
 }
