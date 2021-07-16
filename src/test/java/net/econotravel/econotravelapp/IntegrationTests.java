5df4b166c5520e4ebd784a7a06c39143eee742c1
Javi <javi@jsrois.net>
2021-07-16 16:11:59 +0200

    Fixed integration test

--- src/test/java/net/econotravel/econotravelapp/IntegrationTests.java
+++ src/test/java/net/econotravel/econotravelapp/IntegrationTests.java
@@ -33,14 +33,17 @@ public class IntegrationTests {
 
         mockMvc.perform(get("/experiences"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[*]", hasSize(1)))
+                .andExpect(jsonPath("$[*]", hasSize(2)))
                 .andExpect(jsonPath("$[0].name", equalTo("Paseo en bicicleta por el Montseny")))
-                .andExpect(jsonPath("$[0].pricePerPerson", equalTo(13.29)));
+                .andExpect(jsonPath("$[0].pricePerPerson", equalTo(13.29)))
+                .andExpect(jsonPath("$[1].name", equalTo("De la huerta a la cocina")))
+                .andExpect(jsonPath("$[1].pricePerPerson", equalTo(7.20)));
     }
 
     private void loadFakeData() {
         List<Experience> experienceList = List.of(
-                new Experience(0L, "Paseo en bicicleta por el Montseny", BigDecimal.valueOf(13.29))
+                new Experience(1L, "Paseo en bicicleta por el Montseny", BigDecimal.valueOf(13.29)),
+                new Experience(2L, "De la huerta a la cocina", BigDecimal.valueOf(7.20))
         );
         fakeExperienceRepository.saveAll(experienceList);
     }
