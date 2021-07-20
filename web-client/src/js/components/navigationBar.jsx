01da7343f7f003b65773523ad909335289f46c7d
yassscoder <yasminlcg@gmail.com>
2021-07-20 15:30:27 +0200

    cambios estilo navBar

--- web-client/src/js/components/navigationBar.jsx
+++ web-client/src/js/components/navigationBar.jsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import {Link} from "react-router-dom";
 
 export const NavigationBar = () => (
-    <nav>
+    <nav className={"navigationBar"}>
         <Link to={"/experiencias"}>Experiencias</Link>
         <Link to={"/quienessomos"}>Quiénes Somos</Link>
-        <Link to={"/reservas"}>Reservas</Link>
+        <Link to={"/reservas"} >Reservas</Link>
     </nav>
 )
