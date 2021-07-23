54d4852e36c83b2c6431b17a2b1ebf634ef743f2
Alisa <filonenkoalisa@gmail.com>
2021-07-23 15:10:52 +0200

    cards multiplicar

--- web-client/src/js/components/Experiencias.jsx
+++ web-client/src/js/components/Experiencias.jsx
@@ -22,6 +22,106 @@ export const Experiencias = () => <section>
             </div>
         </div>
     </div>
+
+    <div className="card">
+        <div className="card-header">
+            <div className="profile">
+                <span className="letter">Hola</span>
+            </div>
+            <div className="card-title-group">
+                <h5 className="card-title">Hola</h5>
+                <div className="card-date">Hola</div>
+            </div>
+        </div>
+        <img className="card-image" src={montseny} alt="Logo" />
+        <div className="card-text">Hola</div>
+        <div className="card-like-bar">
+
+            <div className="like-text">
+                <b>Hola</b> kişi bu tarifi beğendi.
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-header">
+            <div className="profile">
+                <span className="letter">Hola</span>
+            </div>
+            <div className="card-title-group">
+                <h5 className="card-title">Hola</h5>
+                <div className="card-date">Hola</div>
+            </div>
+        </div>
+        <img className="card-image" src={montseny} alt="Logo" />
+        <div className="card-text">Hola</div>
+        <div className="card-like-bar">
+
+            <div className="like-text">
+                <b>Hola</b> kişi bu tarifi beğendi.
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-header">
+            <div className="profile">
+                <span className="letter">Hola</span>
+            </div>
+            <div className="card-title-group">
+                <h5 className="card-title">Hola</h5>
+                <div className="card-date">Hola</div>
+            </div>
+        </div>
+        <img className="card-image" src={montseny} alt="Logo" />
+        <div className="card-text">Hola</div>
+        <div className="card-like-bar">
+
+            <div className="like-text">
+                <b>Hola</b> kişi bu tarifi beğendi.
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-header">
+            <div className="profile">
+                <span className="letter">Hola</span>
+            </div>
+            <div className="card-title-group">
+                <h5 className="card-title">Hola</h5>
+                <div className="card-date">Hola</div>
+            </div>
+        </div>
+        <img className="card-image" src={montseny} alt="Logo" />
+        <div className="card-text">Hola</div>
+        <div className="card-like-bar">
+
+            <div className="like-text">
+                <b>Hola</b> kişi bu tarifi beğendi.
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-header">
+            <div className="profile">
+                <span className="letter">Hola</span>
+            </div>
+            <div className="card-title-group">
+                <h5 className="card-title">Hola</h5>
+                <div className="card-date">Hola</div>
+            </div>
+        </div>
+        <img className="card-image" src={montseny} alt="Logo" />
+        <div className="card-text">Hola</div>
+        <div className="card-like-bar">
+
+            <div className="like-text">
+                <b>Hola</b> kişi bu tarifi beğendi.
+            </div>
+        </div>
+    </div>
     );
 
 
