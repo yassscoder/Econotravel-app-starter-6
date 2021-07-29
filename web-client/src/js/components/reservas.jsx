074d3dcc670d13d624908039aba604ff8ed71c20
Sonia Mestre <mestre.sonia@gmail.com>
2021-07-29 14:55:15 +0200

    formulario con react hooks

--- web-client/src/js/components/reservas.jsx
+++ web-client/src/js/components/reservas.jsx
@@ -1,55 +1,66 @@
-import * as React from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import { useForm } from 'react-hook-form';
 
-export const Reservas  = () => <section>
+export function Reservas() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [submitted, setsubmitted] = useState(false);
+    const onSubmit = data => setsubmitted(true);
+    console.log(errors);
 
-    <div className="fondo">
-        <form id="contact" action="" method="post">
-            <h3>Formulario de reserva</h3>
-            <h4>Rellena los datos para realizar la reserva</h4>
-            <fieldset>
-                <input placeholder="Tu nombre" type="text" tabIndex="1" required autoFocus />
-            </fieldset>
-            <fieldset>
-                <input placeholder="Tu dirección de Email" type="email" tabIndex="2" required />
-            </fieldset>
-            <fieldset>
-                <input placeholder="Tu número de móvil (opcional)" type="tel" tabIndex="3" required />
-            </fieldset>
-           <fieldset>
-                <select class="desplegable" type="text" tabIndex="4" required id="experiencia" name="experiencia">
+    return (
+        <div className="fondo">
+            <form id="contact" onSubmit={handleSubmit(onSubmit)}>
+                <h3>Formulario de reserva</h3>
+                <h4>Rellena los datos para realizar la reserva</h4>
+                <fieldset>
+                    <input type="text" placeholder="Nombre" {...register("Nombre", {required: true, maxLength: 80})} />
+                    {errors.Nombre?.type === 'required' && "No te olvides de rellenar este campo"}
+                </fieldset>
+                <fieldset>
+                    <input type="text" placeholder="Apellidos" {...register("Apellidos", {required: true, maxLength: 100})} />
+                    {errors.Apellidos?.type === 'required' && "Campo obligatorio"}
+                </fieldset>
+                <fieldset>
+                    <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+                    {errors.Nombre?.type === 'required' && "mail para enviarte la confirmación"}
+                </fieldset>
+                <fieldset>
+                    <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+                    {errors.Nombre?.type === 'required' && "Sólo te llamaremos por una urgencia"}
+                </fieldset>
+                <fieldset>
+                    <select className="desplegable" {...register("Experiencias", { required: true })}>
                     <option value={"experiencia"}> Selecciona tu experiencia </option>
-                    <option value="1"> Montseny -- 250€ --  </option>
-                    <option value="2"> Vela -- 280€ -- </option>
-                    <option value="3"> Modernismo -- 200€ -- </option>
-                    <option value="4"> Huerto -- 145€ -- </option>
-                    <option value="5"> Montserrat --125€ -- </option>
+                    <option value="Experiencia Montseny -- 250€ --">Experiencia Montseny -- 250€ --</option>
+                    <option value=" Experiencia Montserrat --125€ --"> Experiencia Montserrat --125€ --</option>
+                    <option value=" Experiencia Velero -- 280€ --"> Experiencia Velero -- 280€ --</option>
+                    <option value=" Experiencia Horticultura -- 145€ --"> Experiencia Horticultura -- 145€ --</option>
+                    <option value=" Experiencia Colonia Güell -- 50€ --"> Experiencia Colonia Güell -- 50€ --</option>
+                    <option value=" Experiencia Modernismo -- 200€ --"> Experiencia Modernismo -- 200€ --</option>
                 </select>
-            </fieldset>
-            <fieldset>
-                <select class="desplegable" type="text" tabIndex="4" required id="experiencia" name="experiencia">
+                    {errors.Nombre?.type === 'required' && "Selecciona una experiencia"}
+                </fieldset>
+                <fieldset>
+                    <select className="desplegable" {...register("Personas", { required: true })}>
                     <option select value={"experiencia"}> Número de personas </option>
-                    <option value="1"> 1 </option>
-                    <option value="2"> 2 </option>
-                    <option value="3"> 3 </option>
-                    <option value="4"> 4 </option>
-                    <option value="5"> 5 </option>
-                    <option value="6"> 6 </option>
+                    <option value="1">1</option>
+                    <option value=" 2"> 2</option>
+                    <option value=" 3"> 3</option>
+                    <option value=" 4"> 4</option>
+                    <option value=" 5"> 5</option>
+                    <option value=" 6"> 6</option>
                 </select>
-            </fieldset>
+                    {errors.Nombre?.type === 'required' && "Selección obligatoria"}
+                </fieldset>
+                <fieldset>
+{
+               <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
 
-            <fieldset>
-                <textarea placeholder="Type your message here...." tabIndex="5" required></textarea>
-            </fieldset>
+}
 
-            <fieldset>
-                <Link to={"/Booking-done"} >
-                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-                </Link>
-            </fieldset>
+                </fieldset>
 
-        </form>
-    </div>
-
-
-</section>
+            </form>
+        </div>
+    );
+}
